e7ece4a24859b84aab67a623e7a54c7c5502670f
xeureka <156827630+xeureka@users.noreply.github.com>
2026-01-22 09:19:58 +0300

    fix: doc issue on exclude.ts

--- challenges/exclude.ts
+++ challenges/exclude.ts
@@ -1,12 +1,5 @@
 // Implement the built in Exclude<T,U>
 
-
-
-// write your apprach right here esti
-
-// iterate on the keyof the T
-// and exlude the ones we have in the K
-
 type MyExclude<T,U> = T extends U ? never: T
 
 type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' , 'c'
