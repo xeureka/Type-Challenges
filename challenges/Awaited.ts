a16a71b45799c4a1f54b5bae9809e704b6060bd0
xeureka <156827630+xeureka@users.noreply.github.com>
2026-01-23 15:31:47 +0300

    Awaited

---
+++ challenges/Awaited.ts
@@ -0,0 +1,7 @@
+// If we have a type which is a wrapped type like Promise, how can we get the type which is insde the wrapped type ?
+
+type ExampleType = Promise<string>
+
+type MyAwaited<A> = A extends Promise<infer T> ? T:never 
+
+type Result = MyAwaited<ExampleType> // string
