806372dcd909548cf50f0ac529351a1c1e261462
xeureka <156827630+xeureka@users.noreply.github.com>
2026-01-24 09:04:38 +0300

    Parameters

---
+++ challenges/Parameters.ts
@@ -0,0 +1,7 @@
+// Implement the build in parameters generic withotu using it
+
+type MyParameters<T extends (...args:any) => any > = T extends (...args: infer P) => any ? P:never;
+
+const foo = (arg1: string, arg2: number): void => {}
+
+type FunctionParameterType = MyParameters<typeof foo> // [arg1: string, arg2: number]
