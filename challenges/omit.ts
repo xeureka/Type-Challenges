4449e10b0915fbbc6dfc60f4eafb626767a76603
xeureka <156827630+xeureka@users.noreply.github.com>
2026-02-09 08:08:42 +0300

    Omit

---
+++ challenges/omit.ts
@@ -0,0 +1,16 @@
+// Implement the built in Omit<T,K> generic w/out using it 
+// constructure a type by picking all properies from T and then remving K 
+
+interface Todo {
+    title: string;
+    description: string;
+    completed: boolean
+}
+
+type MyOmit<T, K extends keyof T> = {[P in keyof T as P extends K ? never: P] :T[P]}
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+const todo: TodoPreview = {
+    completed: false
+}
