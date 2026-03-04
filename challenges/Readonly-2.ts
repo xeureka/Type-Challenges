3062ed671e0f2a5d58c37d84c0b73545ca0eeb68
xeureka <156827630+xeureka@users.noreply.github.com>
2026-03-04 14:19:33 +0300

    Readonly 2

---
+++ challenges/Readonly-2.ts
@@ -0,0 +1,21 @@
+type MyReadonly2<T, K extends keyof T> = {
+  readonly [key in keyof T as key extends K ? key : never]: T[key];
+} & {
+  [key in keyof T as key extends K ? never : key]: T[key];
+};
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+const todo: MyReadonly2<Todo, "title" | "description"> = {
+  title: "Hey",
+  description: "foobar",
+  completed: false,
+};
+
+todo.title = "Hello"; // Error: cannot reassign a readonly property
+todo.description = "barFoo"; // Error: cannot reassign a readonly property
+todo.completed = true; // OK
