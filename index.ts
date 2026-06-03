33ed67269a57de86bc76f47eff4cd7a5ecf94426
xeureka <156827630+xeureka@users.noreply.github.com>
2026-06-03 21:09:25 +0300

    new start again

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-type MyParameters<T extends (...args: any[]) => any> = [
-  T extends (...args: infer R) => any ? R : never,
-];
 
-const foo = (arg1: string, arg2: number): void => {};
-
-type FunctionParamsType = MyParameters<typeof foo>; // [arg1: string, arg2: number]
